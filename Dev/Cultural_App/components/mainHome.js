50088b1f42bd9ce4c336b64f81a3e8811783ecff
bum1058 <bum1058@naver.com>
2022-11-08 16:59:08 +0900

    mainhome.js수정
    mainhome.js수정

--- Dev/Cultural_App/components/mainHome.js
+++ Dev/Cultural_App/components/mainHome.js
@@ -16,31 +16,57 @@ import {
 import SelectList from 'react-native-dropdown-select-list';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { color } from 'react-native-reanimated';
+import { Directions } from 'react-native-gesture-handler';
+
 
 const mainHome = ({navigation}) => {
     const [searching,setsearchdata] = useState("");
+    const [showimagetext, setshowimagetext]= useState(true);
+
+    
 
     const data= [
       {key:'1', value:'이상설 생가'},
       {key:'2', value:'김유신 생가'},
       {key:'3', value:'농다리'},
     ];
+
+   
     return(
       <SafeAreaView style={styles.SafeAreaViewStyles}>
         <View style = {styles.viewstyle}>
         <SelectList data={data} 
         setSelected={setsearchdata}
         onSelect={()=> alert(searching)}/>
+
        
         </View >
         <View style={styles.middleviewstyle}>
-          <Text style={styles.textsetting}>우리동네 문화제</Text>
-          <Button title='내주위 문화제' style={styles.button}></Button>
-          <Button title='진천 문화제' style={styles.button}></Button>
-          <Button title='내주위 문화제' style={styles.button}></Button>
-          <Button title='내주위 문화제' style={styles.button}></Button>
-        
+          <View style={styles.sideview}>
+            <Button title='내주위 문화제' style={styles.button}></Button>
+            
+          </View>
+          <SafeAreaView>
+          <ScrollView style={styles.sideview}>
+            <Button title='진천 문화제' style={styles.button} onPress={() =>setshowimagetext(!showimagetext)}></Button>
+            {
+              showimagetext ? (
+                <Image source={require('../image/brige.jpg')} style={styles.imagestyle}/>
+               
+              ) : null
+            }
+            {
+              showimagetext ? (
+              <Text>안녕하세요</Text>
+               
+              ) : null
+            }
+          
+          </ScrollView>
+          </SafeAreaView>
         </View>
+        
       </SafeAreaView>
     )
 }
@@ -49,13 +75,15 @@ const styles = StyleSheet.create({
     viewstyle:{
       paddingHorizontal: 20,
       paddingVertical: 50,
-      height: 30,
+      height: '30%',
       flex: 0.3,
-      backgroundColor:'blue',
+      
     },
     middleviewstyle:{
       backgroundColor: 'red',
+    
       flex:1,
+      marginTop: 0,
     },
     SafeAreaViewStyles:{
       flex:1,
@@ -72,11 +100,14 @@ const styles = StyleSheet.create({
       fontSize:20,
     },
     button: {
-      margin: 10,
-      width: "50%",
-      height: "50%",
-
+      alignItems: 'center',
+     
     },
+    sideview: {
+      width:'100%',
+      height:'50%',
+
+    }
   });
   
 
