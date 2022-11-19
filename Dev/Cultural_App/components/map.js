5a98c2b8f6207628e2589424f703edfa9d5c77b0
beomsu park <beomsu0925@gmail.com>
2022-11-19 12:53:46 +0900

    구글맵api적용(수정중)

--- Dev/Cultural_App/components/map.js
+++ Dev/Cultural_App/components/map.js
@@ -1,22 +1,24 @@
-import React,{useState}from 'react';
+import React from "react";
+import { View, Text } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-import {
-  SafeAreaView, 
-  StyleSheet,
-  View,
-  FlatList,
-  Item,
-  TextInput,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-  Text,
-  Button,
-} from 'react-native';
-
-const map = () => {
-
-
+function App() {
+  return (
+    <>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    </>
+  );
 }
 
-export default map;
+export default App;
