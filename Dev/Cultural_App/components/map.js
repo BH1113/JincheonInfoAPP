6b0bf50ae44e0f46bd812616bd805fa905dceecf
beomsu park <beomsu0925@gmail.com>
2022-11-29 01:16:55 +0900

    맵 권한및 현재위치

--- Dev/Cultural_App/components/map.js
+++ Dev/Cultural_App/components/map.js
@@ -1,24 +1,93 @@
-import React from "react";
-import { View, Text } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React, { Component } from "react";
+// global.currentScreenIndex = 'Dashboard';
+//Import all required component
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Modal,
+  TouchableHighlight,
+  StatusBar,
+  FlatList,
+  ActivityIndicator,
+  Button,
+  NetInfo,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import Geolocation from "@react-native-community/geolocation";
 
-function App() {
-  return (
-    <>
-      <View style={{ flex: 1 }}>
+const MyStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+);
+   
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      },
+    };
+  }
+
+  componentDidMount() {
+    const { navigation } = this.props;
+    Geolocation.getCurrentPosition((info) => {
+      let lat = info.coords.latitude;
+      let long = info.coords.longitude;
+
+      console.log(lat);
+      console.log(long);
+      var initialRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      };
+      this.setState({ initialPosition: initialRegion });
+    });
+  }
+
+  render() {
+    const { modalVisible } = this.state;
+    const { navigate } = this.props.navigation;
+    // const token_data = token_value();
+    // console.log("token_homes_2st"+token_data);
+
+    return (
+      <View style={styles.container}>
         <MapView
-          style={{ flex: 1 }}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
+          style={styles.map}
+          initialRegion={this.state.initialPosition}
+          showsUserLocation={true}
         />
       </View>
-    </>
-  );
+    );
+  }
 }
+export default Dashboard;
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    fontWeight: "400",
+    color: "#000",
+    fontFamily:
+      "Inter UI,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif",
+  },
+
+  map: {
+    height: 610,
+    width: 415,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
