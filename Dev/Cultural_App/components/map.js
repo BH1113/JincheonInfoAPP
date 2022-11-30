dbbd2d53e9005b7202f0e308c9b0fad3a6f5ece4
BH1113 <minee519@gmail.com>
2022-11-30 12:08:52 +0900

    map update

--- Dev/Cultural_App/components/map.js
+++ Dev/Cultural_App/components/map.js
@@ -1,47 +1,60 @@
-import React, {useState} from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {Component} from 'react';
+// global.currentScreenIndex = 'Dashboard';
+//Import all required component
+import {View, StyleSheet} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  FlatList,
-  Item,
-  TextInput,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-  Text,
-  Button,
-} from 'react-native';
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      initialPosition: {
+        latitude: 36.85477746864833,
+        longitude: 127.43554725015466,
+        latitudeDelta: 0.25,
+        longitudeDelta: 0.15,
+      },
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={this.state.initialPosition}
+          showsUserLocation={true}>
+          <Marker
+            coordinate={{
+              latitude: 36.861788908031855,
+              longitude: 127.48033416327694,
+            }}
+          />
+          <Marker
+            coordinate={{
+              latitude: 36.826156484755614,
+              longitude: 127.49318372154447,
+            }}
+          />
+          <Marker
+            coordinate={{
+              latitude: 36.85320365766268,
+              longitude: 127.42283133188303,
+            }}
+          />
+        </MapView>
+      </View>
+    );
+  }
+}
+export default Dashboard;
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    flex: 1,
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    width: '100%',
+    height: '100%',
   },
 });
-const map = () => {
-  return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0112,
-        }}
-      />
-    </View>
-  );
-};
-
-export default map;
