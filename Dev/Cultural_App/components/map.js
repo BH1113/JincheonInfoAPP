6456d8b8faceeec11c3f05335cf4c536e1a0b7ae
beomsu park <beomsu0925@gmail.com>
2022-12-01 17:36:14 +0900

    마커 추가

--- Dev/Cultural_App/components/map.js
+++ Dev/Cultural_App/components/map.js
@@ -21,11 +21,15 @@ class Dashboard extends Component {
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          initialRegion={this.state.initialPosition}
           showsUserLocation={true}>
-         <Marker coordinate={{ latitude: 36.861788908031855, longitude: 127.48033416327694 }} />
-         <Marker coordinate={{ latitude: 36.826156484755614, longitude: 127.49318372154447 }} />
-         <Marker coordinate={{ latitude: 36.85320365766268, longitude: 127.42283133188303 }} />
+         <Marker coordinate={{ latitude: 36.861788908031855, longitude: 127.48033416327694 }} /> 
+         <Marker coordinate={{ latitude: 36.826156484755614, longitude: 127.49318372154447 }} /> 
+         <Marker coordinate={{ latitude: 36.85320365766268, longitude: 127.42283133188303 }} /> 
+         <Marker coordinate={{ latitude: 36.8887086, longitude: 127.4130883 }} /> 
+         <Marker coordinate={{ latitude: 36.798240, longitude: 127.410445 }} /> 
+         <Marker coordinate={{ latitude: 36.854796, longitude: 127.451374 }} />
+         <Marker coordinate={{ latitude: 36.835288, longitude: 127.551342 }} /> 
+         <Marker coordinate={{ latitude: 36.839424, longitude: 127.341038 }} /> 
          </MapView>
       </View>
     );
@@ -36,10 +40,10 @@ export default Dashboard;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1 , flexDirection: 'column-reverse'
   },
   map: {
     width: "100%",
-    height: "100%"
+    height: "96%"
   }
 });
